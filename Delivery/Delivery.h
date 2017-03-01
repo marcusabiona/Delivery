e8671dc8c9ae0c77f41430ce82c055a27a8a89d6
Aleksei Zaikin <aleksi.zaikin@gmail.com>
2017-03-02 00:09:26 +0300

    Initial Commit

---
+++ Delivery/Delivery.h
@@ -0,0 +1,19 @@
+//
+//  Delivery.h
+//  Delivery
+//
+//  Created by Aleksei Zaikin on 02/03/2017.
+//  Copyright © 2017 Aleksei Zaikin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Delivery.
+FOUNDATION_EXPORT double DeliveryVersionNumber;
+
+//! Project version string for Delivery.
+FOUNDATION_EXPORT const unsigned char DeliveryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Delivery/PublicHeader.h>
+
+
